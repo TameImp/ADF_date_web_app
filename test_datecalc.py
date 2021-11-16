af7d4e78c72a11341f615f29ed1f267f4e521a0f
TameImp <matthew.rance@immediate.co.uk>
2021-11-16 11:55:17 +0000

    comment: add comments for each unit test

--- test_datecalc.py
+++ test_datecalc.py
@@ -1,15 +1,18 @@
 from datetime import datetime
 from datecalc import duration, when
 
+# basic test to ensure pytest running as expected
 def test_func():
     assert 5==5
 
+# For loop unit test to check start and end dates recognised as strings (and therefore requiring conversion to datetimes)
 def test_duration():
         start_date = "21,12,2021"
         end_date = "26,12,2021"
         for obj in (start_date, end_date):
             assert isinstance(obj, str)
 
+# Unit test 1 on "test_duration" function to test if start and end date are the same, result of the function is 0
 def test_duration1():
         start_date = "21,12,2021"
         end_date = "21,12,2021"
@@ -18,6 +21,7 @@ def test_duration1():
         result = duration(start_date, end_date)
         assert result == 0
 
+# Unit test 2 on "test_duration" function to test if start date = "21,12,2021" and end date = "26,12,2021" then result of function is 5
 def test_duration2():
         start_date = "21,12,2021"
         end_date = "26,12,2021"
@@ -26,6 +30,7 @@ def test_duration2():
         result = duration(start_date, end_date)
         assert result == 5
 
+# Unit test 3 on "test_duration" function to test if days_between is set as 0 then the result of the function is == to the start_date
 def test_when1():
     start_date = "21,12,2021"
     days_between = 0
@@ -34,6 +39,7 @@ def test_when1():
     result.strftime("%d,%b,%Y")
     assert result == datetime.strptime('2021-12-21 00:00:00', "%Y-%m-%d %H:%M:%S")
 
+# Unit test 4 on "test_duration" function to test if days_between is 5 and start_date = "21,12,2021" then result of function = '2021-12-26 00:00:00'
 def test_when2():
     start_date = "21,12,2021"
     days_between = 5
@@ -41,21 +47,3 @@ def test_when2():
     result = when(start_date, days_between)
     result.strftime("%d,%b,%Y")
     assert result == datetime.strptime('2021-12-26 00:00:00', "%Y-%m-%d %H:%M:%S")
-
-
-# test_duration("21,12,2021", "22,12,2021")
-
-
-# # test if data types in 
-# def test_app_dtype():
-#     start_date = '20-10-2017'
-#     end_date = '20-10-2018'
-    
-#     # duration(start_date, end_date)
-#     # assert isinstance(start_date,str)
-
-# # number = 10
-# # print(isinstance(number,int))
-#     print(type(duration))
-
-# print(isinstance(duration(10,11,str))
